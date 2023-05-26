42ff7e38bdd57017713bdcd34dfe5626929a2e79
david-leverenz <david.leverenz@gmail.com>
2023-05-26 18:46:07 -0500

    created files and folders

---
+++ setupstructure.bat
@@ -0,0 +1,11 @@
+echo "Hello">index.html
+mkdir assets
+mkdir assets\css
+mkdir assets\script
+mkdir assets\images
+cd assets\css
+echo "Hello">style.css
+cd..
+cd..
+cd assets\script
+echo "Hello">script.js
