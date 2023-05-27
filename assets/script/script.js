421dff02ee0e6a46bc2beb8775f0fe63e609886f
david-leverenz <david.leverenz@gmail.com>
2023-05-27 17:51:12 -0500

    pseudo coded the app started the api docs

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,25 @@
-var cityInput = document.querySelector("#searchInput");
+var submitEl = document.querySelector("#submit-form");
+var cityInput = document.querySelector("#search-input");
 var currentWeather = document.querySelector("#current-weather");
 var fiveDay = document.querySelector("#five-day");
 var cityLink = document.querySelector("#city-link");
+
+// sample api call
+//http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=44be570f60fd1ef1f012456a39e5a0ff
+
+// API to make a lat lon call from city name
+// http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=44be570f60fd1ef1f012456a39e5a0ff
+// Comes back in an array "name": "City Name"; "lat"= value; "lon"= value
+
+// Current weather call
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=44be570f60fd1ef1f012456a39e5a0ff
+
+// "main.temp"; "wind.speed"; "main.humidity"
+
+// five day call
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=appid=44be570f60fd1ef1f012456a39e5a0ff
+
+// 
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -23,4 +41,28 @@ var buttonClickHandler = function (event) {
     // will be links to the searched cities
 }
 
+var getWeather =  function (city) {
+    var apiURL = "https://the weather link";
 
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayWeather(data.items, the items);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+        });
+};
+
+var displayWeather = function(something) {
+    if (Weather.length ===0) {
+        Let them know nothing was formSubmitHandler;
+        return;
+    }
+}
+
+// save the input to local storage and then display it in the searched cities section
+
+submitEl.addEventListener("submit", formSubmitHandler);
+cityLink.addEventListener("click", formSubmitHandler);
