a405162cdfb16397faf8af36a98c508f8d84cade
david-leverenz <david.leverenz@gmail.com>
2023-05-27 15:35:56 -0500

    html looks good started js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,26 @@
-console.log("Hello");
+var cityInput = document.querySelector("#searchInput");
+var currentWeather = document.querySelector("#current-weather");
+var fiveDay = document.querySelector("#five-day");
+var cityLink = document.querySelector("#city-link");
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    var cityName = cityInput.value.trim();
+
+    if (cityName) {
+        getWeather(cityName);
+
+        currentWeather.textContent="";
+        fiveDay.textContent="";
+        cityInput="";
+        } else {
+            alert("Please enter a city name.");
+        }
+};
+
+var buttonClickHandler = function (event) {
+    // will be links to the searched cities
+}
 
 
