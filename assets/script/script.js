2a7adadc78e9999d1e3b6ac4ef8ffe28baa85ca5
david-leverenz <david.leverenz@gmail.com>
2023-05-29 12:07:48 -0500

    lat lon is geting fetched!

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,13 @@
 var submitEl = document.querySelector("#submit-form");
 var cityInput = document.querySelector("#search-input");
 var currentWeather = document.querySelector("#current-weather");
+var currentTemp = document.querySelector("#current-temp");
+var currentWind = document.querySelector("#current-wind");
+var currentHumidity = document.querySelector("#current-humidity");
 var fiveDay = document.querySelector("#five-day");
+var fiveTemp = document.querySelector("#five-temp");
+var fiveHumidity = document.querySelector("#five-humidity");
+var fiveWind = document.querySelector("#five-wind");
 var cityLink = document.querySelector("#city-link");
 
 // sample api call
@@ -19,7 +25,8 @@ var cityLink = document.querySelector("#city-link");
 // five day call
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=appid=44be570f60fd1ef1f012456a39e5a0ff
 
-// 
+// loop "dt_txt", "main.temp", "wind.speed"; "main.humidity"
+
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -27,42 +34,56 @@ var formSubmitHandler = function (event) {
     var cityName = cityInput.value.trim();
 
     if (cityName) {
-        getWeather(cityName);
+        getLatLon(cityName);
 
-        currentWeather.textContent="";
-        fiveDay.textContent="";
-        cityInput="";
-        } else {
-            alert("Please enter a city name.");
-        }
+        currentWeather.textContent = "";
+        fiveDay.textContent = "";
+        cityInput = "";
+    } else {
+        alert("Please enter a city name.");
+    }
 };
 
-var buttonClickHandler = function (event) {
-    // will be links to the searched cities
-}
+// var buttonClickHandler = function (event) {
+//     // will be links to the searched cities
+// }
 
-var getWeather =  function (city) {
-    var apiURL = "https://the weather link";
+// getWeather();
 
-    fetch(apiURL).then(function(response) {
+// var cityName = "London";
+
+var getLatLon = function (city) {
+    var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=44be570f60fd1ef1f012456a39e5a0ff";
+
+    fetch(apiURL).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                displayWeather(data.items, the items);
+                for (var i = 0; i < data.length; i++) {
+                    var latitude = data[i].lat;
+                    var longitude = data[i].lon;
+                    console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+                }
+
+
+
             });
         } else {
             alert("Error: " + response.statusText);
         }
-        });
+    });
 };
 
-var displayWeather = function(something) {
-    if (Weather.length ===0) {
-        Let them know nothing was formSubmitHandler;
-        return;
-    }
-}
+// var LatLon = function (latitude, longitude) {
+//     if (latitude.length === 0) {
+//         alert("No latitude found.");
+//     } else if (longitude.length === 0) {
+//         alert("No longitude found.");
+//     }
+//     return;
+// }
+
 
 // save the input to local storage and then display it in the searched cities section
 
 submitEl.addEventListener("submit", formSubmitHandler);
-cityLink.addEventListener("click", formSubmitHandler);
+// cityLink.addEventListener("click", buttonClickHandler);
